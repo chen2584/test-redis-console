d936715280baee4d1427adabceeb0a02b0f7a972
Worameth Semapat <worameth.semapat@gmail.com>
2018-06-20 09:23:26 +0700

    Add try catch

--- Program.cs
+++ Program.cs
@@ -9,38 +9,46 @@ namespace testRedisConsole
     {
         static void Main(string[] args)
         {
-            using(RedisClient redisClient = new RedisClient("redis://clientid:Chenz2584@localhost:6379"))
+            try
             {
-                IRedisTypedClient<Person> redisUser = redisClient.As<Person>();
-                
-                var rick = new Person { Id = "Rick", Name = "Rick @ code with intent" };
-                var backy = new Person { Id = "Backy", Name = "Backy @ becy,com" };
-                //var chen = new Person { Id = redisUser.GetNextSequence(), Name = "Chen @ becy,com" };
-
-                redisUser.Store(rick);
-                redisUser.Store(backy);
-                
-                var allThePeople = redisUser.GetAll();
-                Console.WriteLine("--- Get All People ---");
-                foreach(var people in allThePeople)
+                using (RedisClient redisClient = new RedisClient("redis://clientid:123456789@localhost:6379"))
                 {
-                    Console.WriteLine($"Id: {people.Id}- Name: {people.Name} ");
+                    IRedisTypedClient<Person> redisUser = redisClient.As<Person>();
+
+                    var rick = new Person { Id = "Rick", Name = "Rick @ code with intent" };
+                    var backy = new Person { Id = "Backy", Name = "Backy @ becy,com" };
+                    //var chen = new Person { Id = redisUser.GetNextSequence(), Name = "Chen @ becy,com" };
+
+                    redisUser.Store(rick);
+                    redisUser.Store(backy);
+
+                    var allThePeople = redisUser.GetAll();
+                    Console.WriteLine("--- Get All People ---");
+                    foreach (var people in allThePeople)
+                    {
+                        Console.WriteLine($"Id: {people.Id}- Name: {people.Name} ");
+                    }
+                    //Console.WriteLine(allThePeople.Dump()); //return json format
+
+                    Console.WriteLine("--- GetByUser ---");
+                    redisClient.SetValue("ChenName", "Worameth Semapat", TimeSpan.FromSeconds(50));
+                    Console.WriteLine("Get Result: " + redisClient.GetValue("ChenName"));
+
+                    Console.WriteLine("--- Get Int ---");
+                    redisClient.Set("testInt", 1);
+                    var getInt = redisClient.Get<int>("testInt");
+                    Console.WriteLine($"Type: {getInt.GetType().Name} - Value: {getInt}");
+
+                    // Set Expire
+                    redisClient.AddItemToList("ChenList", "Test1");
+                    redisClient.AddItemToList("ChenList", "Test2");
+                    redisClient.Expire("ChenList", 30);
                 }
-                //Console.WriteLine(allThePeople.Dump()); //return json format
-
-                Console.WriteLine("--- GetByUser ---");
-                redisClient.SetValue("ChenName", "Worameth Semapat", TimeSpan.FromSeconds(50));
-                Console.WriteLine("Get Result: " + redisClient.GetValue("ChenName"));
-
-                Console.WriteLine("--- Get Int ---");
-                redisClient.Set("testInt", 1);
-                var getInt = redisClient.Get<int>("testInt");
-                Console.WriteLine($"Type: {getInt.GetType().Name} - Value: {getInt}");
-
-                // Set Expire
-                redisClient.AddItemToList("ChenList", "Test1");
-                redisClient.AddItemToList("ChenList", "Test2");
-                redisClient.Expire("ChenList", 30);
+            }
+            catch (RedisException ex) //When server down or invalid password
+            {
+                Console.WriteLine("RedisException");
+                Console.WriteLine(ex.Message);
             }
         }
     }
